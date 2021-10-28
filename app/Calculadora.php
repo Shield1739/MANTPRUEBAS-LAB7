777f83e6f1dd7a8baab54ff08aa910b1752b1a93
Shield1739 <18101480+Shield1739@users.noreply.github.com>
2021-10-28 08:51:22 -0500

    Lab 8

--- app/Calculadora.php
+++ app/Calculadora.php
@@ -5,4 +5,36 @@ class Calculadora
     {
         return $numero1 + $numero2;
     }
+
+    function restar($numero1, $numero2)
+    {
+        return $numero1 - $numero2;
+    }
+
+    function multiplicar($numero1, $numero2)
+    {
+        return $numero1 * $numero2;
+    }
+
+    function dividir($numero1, $numero2)
+    {
+        if ($numero2 == 0)
+        {
+            return new Exception("Division entre 0");
+        }
+
+        return $numero1 / $numero2;
+    }
+
+    function generarArreglo()
+    {
+        $nums = [];
+
+        for ($i = 0; $i < 5; $i++)
+        {
+            $nums[$i] = rand(1, 10);
+        }
+
+        return $nums;
+    }
 }
--- app/CalculadoraTest.php
+++ app/CalculadoraTest.php
@@ -5,10 +5,89 @@ use \PHPUnit\Framework\TestCase;
 
 class CalculadoraTest extends TestCase
 {
-    public function testSumar()
+    public function testSumarProveedor()
+    {
+        return [
+            'Caso 1' => [-1, -1, -2],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, -1],
+            'Caso 4' => [-1, 0, -1]
+        ];
+    }
+
+    /**
+     * @dataProvider testSumarProveedor
+     */
+    public function testSumar($numero1, $numero2, $resultado_esperado)
     {
         $calculadora = new Calculadora();
-        $resultado = $calculadora->sumar(3,3);
-        $this->assertEquals(6, $resultado);
+        $this->assertEquals($resultado_esperado, $calculadora->sumar($numero1, $numero2), "Error en suma");
+        $this->assertSame($resultado_esperado, $calculadora->sumar($numero1, $numero2), "Error en suma (assert same)");
+    }
+
+    public function testRestarProveedor()
+    {
+        return [
+            'Caso 1' => [-1, -1, 0],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 1],
+            'Caso 4' => [-1, 0, -1]
+        ];
+    }
+
+    /**
+     * @dataProvider testRestarProveedor
+     */
+    public function testRestar($numero1, $numero2, $resultado_esperado)
+    {
+        $calculadora = new Calculadora();
+        $this->assertEquals($resultado_esperado, $calculadora->restar($numero1, $numero2), "Error en resta");
+    }
+
+    public function testMultiplicarProveedor()
+    {
+        return [
+            'Caso 1' => [-1, -1, 1],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [2, 3, 6],
+            'Caso 4' => [-5, 2, -10]
+        ];
+    }
+
+    /**
+     * @dataProvider testMultiplicarProveedor
+     */
+    public function testMultiplicar($numero1, $numero2, $resultado_esperado)
+    {
+        $calculadora = new Calculadora();
+        $this->assertEquals($resultado_esperado, $calculadora->multiplicar($numero1, $numero2), "Error en multiplicar");
+    }
+
+    public function testDividirProveedor()
+    {
+        return [
+            'Caso 1' => [-27.3, 4.0, -6.825],
+            'Caso 2' => [-5.0, 2.0, -2.5],
+            'Caso 3' => [6.0, 2.0, 3.0],
+            'Caso 4' => [25.0, 0.0, new Exception("Division entre 0")]
+        ];
+    }
+
+    /**
+     * @dataProvider testDividirProveedor
+     */
+    public function testDividir($numero1, $numero2, $resultado_esperado)
+    {
+        $calculadora = new Calculadora();
+        $this->assertEqualsWithDelta($resultado_esperado, $calculadora->dividir($numero1, $numero2), 0.1, "Error en dividir");
+    }
+
+    public function testGenerarArreglo()
+    {
+        $calculadora = new Calculadora();
+        //$this->assertContains(1, $calculadora->generarArreglo(), "error en generar arreglo");
+        //$this->assertCount(5, $calculadora->generarArreglo(), "error en generar arreglo (assert count)");
+        $this->assertEmpty($calculadora->generarArreglo(), "error en generar arreglo (assert empty)");
+        $this->assertNotEmpty($calculadora->generarArreglo(), "error en generar arreglo (assert not empty)");
     }
 }
